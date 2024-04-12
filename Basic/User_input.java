29ca8e00791a502d2cae3003f3f70c990d027fa7
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2024-04-12 16:08:02 +0530

    input

---
+++ Basic/User_input.java
@@ -0,0 +1,31 @@
+package Basic;
+
+import java.util.Scanner;
+
+/**
+ * 1_User_input
+ */
+public class User_input {
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Initialize sum and count of input elements
+        int sum = 0, count = 0;
+
+        // Check if an int value is available
+        while (sc.hasNextInt()) {
+            // Read an int value
+            int num = sc.nextInt();
+            sum += num;
+            count++;
+        }
+        if (count > 0) {
+            int mean = sum / count;
+            System.out.println("Mean: " + mean);
+        } else {
+            System.out.println(
+                    "No integers were input. Mean cannot be calculated.");
+        }
+    }
+}
