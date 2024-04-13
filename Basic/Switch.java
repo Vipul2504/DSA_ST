8a0355f0c7de528a4dcb7fbba75d6ac8811f04ca
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2024-04-13 21:05:15 +0530

    ch switch

--- Basic/Switch.java
+++ Basic/Switch.java
@@ -4,38 +4,23 @@ import java.util.Scanner;
 
 public class Switch {
     public static void main(String[] args) {
-        int day;
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter a number (1-7): ");
-        day = scanner.nextInt();
-
-        switch (day) {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
-            default:
-                System.out.println("Invalid");
-        }
-
+        System.out.print("Enter a number (1-2): ");
+        int ch = scanner.nextInt();
+        double[] coll = { 1.5, 2.5 };
+        double ans = areaSwitchCase(ch, coll);
+        System.out.println(ans);
         scanner.close();
+    }
+
+    public static double areaSwitchCase(int ch, double[] a) {
+        switch (ch) {
+            case 1:
+                return Math.PI * a[0] * a[0];
+            case 2:
+                return a[0] * a[1];
+            default:
+                return -1;
+        }
     }
 }
