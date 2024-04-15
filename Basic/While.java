77e323363f4e4f66113638565d72165b05fd826d
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2024-04-15 22:28:12 +0530

    Done

---
+++ Basic/While.java
@@ -0,0 +1,24 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class While {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int oddSum = 0;
+        int evenSum = 0;
+
+        while (n != 0) {
+            int digit = n % 10;
+            if (digit % 2 == 0) {
+                evenSum += digit;
+
+            } else {
+                oddSum += digit;
+            }
+            n = n / 10;
+        }
+        sc.close();
+    }
+}
