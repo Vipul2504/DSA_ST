bc1f5b860173b06b8dbf936f814a3ae2ccd8a6bd
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2024-04-20 23:00:42 +0530

    M

--- Basic/FindAreaOfRect.java
+++ Basic/FindAreaOfRect.java
@@ -5,13 +5,9 @@ import java.util.Scanner;
 public class FindAreaOfRect {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int len = sc.nextInt();
-
         int bre = sc.nextInt();
-
         System.out.println(len * bre);
-
         sc.close();
     }
 }
